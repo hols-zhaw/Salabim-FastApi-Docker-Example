dc0d86e1b121a63e9fb8e5761ed342475837a652
hols <5917268+hols-zhaw@users.noreply.github.com>
2025-04-16 10:57:41 +0200

    run several replications

--- app/main.py
+++ app/main.py
@@ -17,7 +17,10 @@ async def run_simulation(
         1.0, description="mean inter-arrival time (exponential distribution)"
     ),
     pt_mean: Optional[float] = Query(
-        100.0, description="mean processing time (exponential distribution)"
+        1.0, description="mean processing time (exponential distribution)"
+    ),
+    n_replications: Optional[int] = Query(
+        1, description="number of replications for the simulation run"
     ),
 ):
     """
@@ -28,23 +31,25 @@ async def run_simulation(
         Simulation run duration.
     - iat_mean: float = 1.0
         Mean inter-arrival time (exponential distribution).
-    - pt_mean: float = None
+    - pt_mean: float = 1.0
         Mean processing time (exponential distribution).
+    - n_replications: int = 1
+        Number of replications for the simulation run.
 
     Returns:
     JSON response with KPIs and time series data.
     """
-    # Prepare keyword arguments for the simulate function
-    simulation_params = {
-        "run_duration": run_duration,
-        "iat_mean": iat_mean,
-        "pt_mean": pt_mean,
-    }
-
-    # Call the simulate function
-    result = simulate(**simulation_params)
-
-    return result
+    return [
+        {
+            "replication_nr": i + 1,
+            **simulate(
+                run_duration=run_duration,
+                iat_mean=iat_mean,
+                pt_mean=pt_mean,
+            ),
+        }
+        for i in range(n_replications)
+    ]
 
 
 if __name__ == "__main__":
